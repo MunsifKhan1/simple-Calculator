069885e6dcfffc62fda0d8c0b6a7b1997bad3c3d
MunsifKhan1 <munsifk597@gmail.com>
2024-10-02 03:50:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// let num=10;
+// num+=5;
+// console.log(num)
+// let num = 10;
+// num-=5;
+// console.log(num)
+// let num=10;
+// num*=5;
+// console.log(num)
+// let num=10;
+// num/=2;
+// console.log(num);
+// let num=10;
+// num %=3;
+// console.log(num)
+// let num = 5;
+// num **=3;
+// console.log(num);
+// let num =10;
+// num++;
+// console.log(num++);
+// console.log(num)
+// let num =10;
+// num--;
+// console.log(num);
+// console.log(num--);
+
+// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
+// for(let i=0 ;i< name.length ;i++){
+//     console.log(name[i]);
+// }
+// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
+// for(let i=0 ;i< name.length ;i++){
+//     console.log(name[i] + " khan");
+// }
+// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
+// for(let i=0 ;i< name.length ;i++){
+//     console.log( "1." + name[i] + " khan");
+// }
+
+let i=14;
+do{
+ console.log(i);
+ i++;
+}
+while(i<20);
