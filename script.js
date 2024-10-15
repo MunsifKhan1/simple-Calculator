7cc67c66d36476a320243bd953bf20634a6f3e93
MunsifKhan1 <munsifk597@gmail.com>
2024-10-14 23:55:43 -0700

    add a index.html all file

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-// let num=10;
-// num+=5;
-// console.log(num)
-// let num = 10;
-// num-=5;
-// console.log(num)
-// let num=10;
-// num*=5;
-// console.log(num)
-// let num=10;
-// num/=2;
-// console.log(num);
-// let num=10;
-// num %=3;
-// console.log(num)
-// let num = 5;
-// num **=3;
-// console.log(num);
-// let num =10;
-// num++;
-// console.log(num++);
-// console.log(num)
-// let num =10;
-// num--;
-// console.log(num);
-// console.log(num--);
-
-// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
-// for(let i=0 ;i< name.length ;i++){
-//     console.log(name[i]);
-// }
-// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
-// for(let i=0 ;i< name.length ;i++){
-//     console.log(name[i] + " khan");
-// }
-// let name =["munsif","khan","ali","ahmad","zeeshan","awais"];
-// for(let i=0 ;i< name.length ;i++){
-//     console.log( "1." + name[i] + " khan");
-// }
-
-let i=14;
-do{
- console.log(i);
- i++;
+const display=document.getElementById("display");
+function appendToDisplay(value){
+display. value += value;
 }
-while(i<20);
+function cleardisplay(){
+    display.value = '';
+}
+function deletelast(){
+    display.value= display.value.slice(0,-1);
+}
+function calculateResult(){
+    try{
+    display.value = eval(display.value);
+}catch (error){
+        display.value=('error')
+        }
+}
+
